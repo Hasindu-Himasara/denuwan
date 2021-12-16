bb1332a6bbf20573b85e03f85a433ce2de2d6177
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-12-16 09:13:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,6 @@ import os
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
-    # from sample_config import Config
-
-# from sample_configmy import Config
 
 from pyrogram import idle
 from pyrogram import Client
